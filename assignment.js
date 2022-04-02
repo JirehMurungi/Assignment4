97809189b0baafc684ff8833920bf17b0f3e12ef
JirehMurungi <jirehmurungi@gmail.com>
2022-04-02 18:05:03 +0300

    call method with arguments

--- assignment.js
+++ assignment.js
@@ -1,12 +1,12 @@
 // Elevator going up
-function Elevator(){
+function Elevator(up){
     for (let floor=1; floor<=4; floor++){
         console.log("Going Up");
         if (floor==4){
             console.log("you have reached floor 4");
         }   
     }
-
+return up;
 }
     
 Elevator ()
